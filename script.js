c058e475e5552c3ed18e61054a3451a0e69015f1
AshfaqSifat <ashfaqs2007@gmail.com>
2023-08-03 15:40:21 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Add your code to this file
+let images = document.querySelectorAll("img");
+let h3 = document.querySelectorAll("h3");
+let h1 = document.querySelector("h1");
+let body = document.querySelector("body");
+let footer = document.querySelector("footer");
+
+for(i = 0; i < images.length; i++) {
+    images[i].style.border = "3px dotted yellow";
+}
+for(i = 0; i < h3.length; i++) {
+    h3[i].style.color = "yellow";
+}
+
+body.style.backgroundColor = "blue";
+h1.style.textDecoration = "underline";
+footer.style.textDecoration = "underline overline";
